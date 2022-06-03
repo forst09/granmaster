e12c35eaba43efa5089993df61b8484c9073b401
Daria <dforst09@gmail.com>
2022-06-03 18:10:32 +0300

    catalog 2 lvl + swiper

--- app/js/full-main.js
+++ app/js/full-main.js
@@ -64,41 +64,25 @@ const swiperMain = new Swiper('.swiper-main', {
     navigation: {
         nextEl: '.main-button-next',
         prevEl: '.main-button-prev',
-    }
-});
-
-const swiperDemanded = new Swiper('.swiper-demanded', {
-    speed: 700,
+    },
     breakpoints: {
-        // when window width is >= 640px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 16,
-        },
-        668: {
-            slidesPerView: 4,
-            spaceBetween: 16,
-        },
         1024: {
-            spaceBetween: 18,
-            slidesPerView: 3.7,
-            navigation: {
-                nextEl: '.demanded-button-next',
-                prevEl: '.demanded-button-prev',
-            },
+            allowTouchMove: false,
         }
     }
 });
 
 const swiperYearsThumbs = new Swiper('.swiper-years-thumbs', {
     speed: 700,
+    slidesPerView: 3.5,
+    spaceBetween: 16,
     navigation: {
         nextEl: '.years-button-next',
         prevEl: '.years-button-prev',
     },
     breakpoints: {
         320: {
-            slidesPerView: 2,
+            slidesPerView: 'auto',
             spaceBetween: 6,
         },
         668: {
@@ -109,9 +93,8 @@ const swiperYearsThumbs = new Swiper('.swiper-years-thumbs', {
             slidesPerView: 3.5,
             spaceBetween: 16,
         }
-    }
+    },
 });
-
 const swiperYears = new Swiper('.swiper-years', {
     speed: 700,
     slidesPerView: 1,
@@ -121,25 +104,87 @@ const swiperYears = new Swiper('.swiper-years', {
     allowTouchMove: false
 });
 
-const swiperInterested = new Swiper('.swiper-interested', {
-    speed: 700,
-    navigation: {
-        nextEl: '.interested-button-next',
-        prevEl: '.interested-button-prev',
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1.05,
-            spaceBetween: 8,
+
+
+// const swiperYearsThumbs = new Swiper('.swiper-years-thumbs', {
+//     speed: 700,
+//     navigation: {
+//         nextEl: '.years-button-next',
+//         prevEl: '.years-button-prev',
+//     },
+//     breakpoints: {
+//         320: {
+//             // slidesPerView: 2,
+//             spaceBetween: 6,
+//         },
+//         668: {
+//             slidesPerView: 3.3,
+//             spaceBetween: 8,
+//         },
+//         1024: {
+//             slidesPerView: 3.5,
+//             spaceBetween: 16,
+//         }
+//     }
+// });
+
+// const swiperYears = new Swiper('.swiper-years', {
+//     speed: 700,
+//     slidesPerView: 1,
+//     thumbs: {
+//         swiper: swiperYearsThumbs,
+//     },
+//     allowTouchMove: false
+// });
+
+const pageWidth = document.documentElement.scrollWidth;
+if (pageWidth>=1024) {
+    const swiperDemanded = new Swiper('.swiper-demanded', {
+        speed: 700,
+        spaceBetween: 18,
+        slidesPerView: 3.7,
+        navigation: {
+            nextEl: '.demanded-button-next',
+            prevEl: '.demanded-button-prev',
         },
-        668: {
-            slidesPerView: 2.01,
-            spaceBetween: 12,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 11,
-            loop: true,
+    });
+
+    const swiperInterested = new Swiper('.swiper-interested', {
+   
+        breakpoints: {
+            1024: {
+                loop: true,
+                slidesPerView: 2,
+                spaceBetween: 11,
+                speed: 700,
+                navigation: {
+                nextEl: '.interested-button-next',
+                prevEl: '.interested-button-prev',
+                },
+            }
         }
-    }
-});
+        
+    });
+}
+// if (pageWidth < 1024) {
+//     swiperDemanded.destroy();
+// }
+
+
+
+// if (pageWidth < 1024) {
+//     swiperInterested.destroy();
+// }
+
+
+
+// window.addEventListener("resize", function () {
+//     const screenWidth = this.innerWidth;
+
+// });
+
+
+// console.log(screenWidth, pageWidth);
+// if (screenWidth >= 1024) {
+    
+// }
